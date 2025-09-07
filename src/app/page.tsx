fca63b80a478dc7f92c009d2c90919ca7dbbd965
Scottiieee <splendidhoodies@gmail.com>
2025-09-07 22:20:45 +0800

    feat: Install shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
 export default function Page() {
-  return (
-    <div className="space-y-4">
-      <p className="font-sans font-light">Erode 300 - Light</p>
-      <p className="font-sans font-normal">Erode 400 - Regular</p>
-      <p className="font-sans font-medium">Erode 500 - Medium</p>
-      <p className="font-sans font-semibold">Erode 600 - SemiBold</p>
-      <p className="font-sans font-bold">Erode 700 - Bold</p>
-    </div>
-  );
+  return <main className=""></main>;
 }

216dae3a23248b61e7c6678cdaafe7e88fe94dac
Scottiieee <splendidhoodies@gmail.com>
2025-09-09 23:58:33 +0800

    feat: chagne to automation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,12 @@ export const metadata: Metadata = {
     template: "%s | Leonexia",
   },
   description:
-    "Leonexia offers expert Web Development in the Philippines along with UI/UX Design, AI Solutions, Cloud Consulting,  tailored for businesses.",
+    "Leonexia offers expert Web Development in the Philippines along with UI/UX Design, Automation Solutions, Cloud Consulting,  tailored for businesses.",
   keywords: [
     "Web Development Philippines",
     "Philippines Web Development Company",
     "UI/UX Design Philippines",
-    "AI Solutions Philippines",
+    "Automation Solutions Philippines",
     "Cloud Solutions Philippines",
     "Software Development Philippines",
   ],
